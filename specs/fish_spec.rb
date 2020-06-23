fc59c76d73c698ede0530cd2a5056ac507a3708c
peaky76 <robertjamespeacock@gmail.com>
2020-06-23 16:54:06 +0100

    created setup and test name

--- specs/fish_spec.rb
+++ specs/fish_spec.rb
@@ -7,5 +7,14 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new()
 
 class TestFish < MiniTest::Test
 
+    def setup()
+        @fish1 = Fish.new("Sammy Salmon")
+        @fish2 = Fish.new("Polly Perch")
+        @fish3 = Fish.new("Tommy Tench")
+    end
+
+    def test_has_name()
+        assert_equal("Sammy Salmon", @fish1.name)
+    end
 
 end
