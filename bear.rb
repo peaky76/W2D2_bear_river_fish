ebe2bb1e3725312c56644a3aa5992a528aaa9fa6
peaky76 <robertjamespeacock@gmail.com>
2020-06-23 17:35:50 +0100

    bear: test take fish pass

--- bear.rb
+++ bear.rb
@@ -11,5 +11,14 @@
     def fish_in_stomach_count()
         return @stomach_contents.count()
     end
+
+    def take_fish(river)
+        # Bear takes random fish from stock in river
+        caught_fish = river.fish_stock.sample()
+        # River loses fish
+        river.remove_fish(caught_fish)
+        # Bear add fish to stomach contents!
+        @stomach_contents << caught_fish 
+    end
     
 end
