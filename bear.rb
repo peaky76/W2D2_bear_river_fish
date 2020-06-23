fd143e5a3b7cb31d018439849843d838bdb6490d
peaky76 <robertjamespeacock@gmail.com>
2020-06-23 17:24:23 +0100

    bear: test empty stomach pass

--- bear.rb
+++ bear.rb
@@ -5,6 +5,11 @@
     def initialize(name, type)
         @name = name
         @type = type
+        @stomach_contents = []
     end
 
+    def fish_in_stomach_count()
+        return @stomach_contents.count()
+    end
+    
 end
